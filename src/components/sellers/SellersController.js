832c953d77f8d2b46705648740197e5f72bece9b
helgir <helgir87@gmail.com>
2016-03-09 14:24:51 +0000

    Added list of users

--- src/components/sellers/SellersController.js
+++ src/components/sellers/SellersController.js
@@ -4,6 +4,11 @@ angular.module("project3App").controller("SellersController",
 function SellersController($scope, AppResource) {
 	// TODO: load data from AppResource! Also, add other methods, such as to
 	// add/update sellers etc.
+
+
+	AppResource.getSellers().success(function(sellers) {
+		$scope.sellers = sellers;
+	});
 	
 	function add(seller) {
 		
