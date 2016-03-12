ddc46a94e12556a59b19744dca2178ac197ea2e8
IronPeak <HrafnOrri1207@gmail.com>
2016-03-12 23:53:53 +0000

    update

--- src/components/resource/AppResource.js
+++ src/components/resource/AppResource.js
@@ -7,24 +7,28 @@
 angular.module("project3App").factory("AppResource",
 function AppResource() {
 
+	var sellerID = 0;
 	// A helper function to create a seller object.
 	// Note that this is only a helper to generate
 	// mock data, and is not a part of our business logic!
-	function createSeller(id, name, category, img) {
+	function createSeller(name, category, img) {
+		sellerID += 1;
 		return {
-			id: id,
+			id: sellerID,
 			name: name,
 			category: category,
 			imagePath: img
 		};
 	}
 
+	var productID = 0;
 	// Another helper function.
-	function createProduct(sellerid, id, productName, price, quantitySold, quantityInStock, path) {
+	function createProduct(sellerid, productName, price, quantitySold, quantityInStock, path) {
+		productID += 1;
 		return {
 			id: sellerid,
 			product: {
-				id: id,
+				id: productID,
 				name: productName,
 				price: price,
 				quantitySold: quantitySold,
@@ -37,35 +41,35 @@ function AppResource() {
 	// Out mock data. Note that this is just to help us
 	// during development!
 	var mockSellers = [
-		createSeller(1, "Hannyrðaþjónusta Hannesar", "Fatnaður", "http://i.imgur.com/OYVpe2W.jpg?fb"),
-		createSeller(2, "Smíðaverkstæði Sigríðar", "Skartgripir", "https://i.imgur.com/ywaPivVh.jpg"),
-		createSeller(3, "Sælgætisgerð Sjonna og Súsí", "Matvörur", "http://i.imgur.com/IuL474x.jpg"),
-		createSeller(4, "Leirkeraverkstæði Lomma", "Keramik", "https://upload.wikimedia.org/wikipedia/commons/6/67/Potter_at_work,_Jaura,_India.jpg")
+		createSeller("Hannyrðaþjónusta Hannesar", "Fatnaður", "http://i.imgur.com/OYVpe2W.jpg?fb"),
+		createSeller("Smíðaverkstæði Sigríðar", "Skartgripir", "https://i.imgur.com/ywaPivVh.jpg"),
+		createSeller("Sælgætisgerð Sjonna og Súsí", "Matvörur", "http://i.imgur.com/IuL474x.jpg"),
+		createSeller("Leirkeraverkstæði Lomma", "Keramik", "https://upload.wikimedia.org/wikipedia/commons/6/67/Potter_at_work,_Jaura,_India.jpg")
 	];
 
 	var mockProducts = [
-		createProduct(1,  1, "Ullarvettlingar",  1899, 500, 12, "http://i.imgur.com/MZOmRnH.jpg"),
-		createProduct(1,  2, "Ullarsokkar",      2199, 488,  9, "http://i.imgur.com/0XKznD4.jpg?1"),
-		createProduct(1,  3, "Trefill",           999, 600, 23, "http://i.imgur.com/50ivFlC.jpg"),
-		createProduct(1,  4, "Sjal",             2399, 120, 65, "https://farm6.static.flickr.com/5205/5298302908_fb75ed8e0a.jpg"),
-		createProduct(1,  5, "Húfa",             1799, 700, 11, "http://purnahandmade.com/media/catalog/product/cache/1/image/ab49223884317513dca074f3bc642368/p/h/phc_malle_08_orwh.jpg"),
-		createProduct(1,  6, "Bjórvettlingar",   2649,  12, 99, "https://img1.etsystatic.com/050/1/5847299/il_214x170.730058347_mt4x.jpg"),
-		createProduct(1,  7, "Jakki",            4499,  23, 14, "http://www.newmanmayahandicraft.com.np/wp-content/uploads/2015/10/woolen-jacket-with-multicolored-design.jpg"),
-		createProduct(1,  8, "Peysa",            5899, 122,  1, "https://upload.wikimedia.org/wikipedia/commons/7/75/Selburose-sweater.jpg"),
-		createProduct(1,  9, "Lambhúshetta",     2499, 322,  4, "https://upload.wikimedia.org/wikipedia/commons/9/9a/Balaclava_3_hole_black.jpg"),
-		createProduct(1, 10, "Buxur",            4299,  73,  5, ""),
-		createProduct(1, 11, "Grifflur",         1299,  98,  9, ""),
-		createProduct(1, 12, "Teppi",             499, 819, 98, "https://pixabay.com/static/uploads/photo/2015/11/07/14/40/fabric-1031932_960_720.jpg"),
-		createProduct(1, 13, "Sokkar",            499, 991, 23, "https://upload.wikimedia.org/wikipedia/commons/4/42/HandKnittedWhiteLaceSock.jpg"),
-		createProduct(1, 14, "Bindi",             899,  25, 22, ""),
-		createProduct(1, 15, "Slaufa",            499, 552, 54, "https://pixabay.com/static/uploads/photo/2015/04/20/21/39/bow-tie-732289_960_720.jpg"),
-		createProduct(1, 16, "Hnéháir sokkar",   2499,  93, 42, "https://upload.wikimedia.org/wikipedia/commons/c/cf/Argyle_(PSF).png"),
-		createProduct(1, 17, "Barnatrefill",      999,  39, 11, "https://upload.wikimedia.org/wikipedia/commons/a/a4/Well-clothed_baby.jpg"),
-		createProduct(1, 18, "Hneppt peysa",     4499,  19,  9, "https://upload.wikimedia.org/wikipedia/commons/4/4d/1940_Trachtenstrickjacke_anagoria.JPG"),
-		createProduct(1, 19, "Hvítir vettlingar", 499, 241,  0, "https://pixabay.com/static/uploads/photo/2014/05/05/22/15/gloves-338614_960_720.jpg"),
-		createProduct(1, 20, "Úlnliðshlífar",    1499,  34,  0, "https://pixabay.com/static/uploads/photo/2015/11/07/17/20/hands-1032312_960_720.jpg"),
-		createProduct(3, 21, "Kókoskúlur",        499, 100, 5000, "https://upload.wikimedia.org/wikipedia/commons/2/2c/Chokladbollar.jpg"),
-		createProduct(3, 22, "Brjóstsykur",       499, 200, 4900, ""),
+		createProduct(1, "Ullarvettlingar",  1899, 500, 12, "http://i.imgur.com/MZOmRnH.jpg"),
+		createProduct(1, "Ullarsokkar",      2199, 488,  9, "http://i.imgur.com/0XKznD4.jpg?1"),
+		createProduct(1, "Trefill",           999, 600, 23, "http://i.imgur.com/50ivFlC.jpg"),
+		createProduct(1, "Sjal",             2399, 120, 65, "https://farm6.static.flickr.com/5205/5298302908_fb75ed8e0a.jpg"),
+		createProduct(1, "Húfa",             1799, 700, 11, "http://purnahandmade.com/media/catalog/product/cache/1/image/ab49223884317513dca074f3bc642368/p/h/phc_malle_08_orwh.jpg"),
+		createProduct(1, "Bjórvettlingar",   2649,  12, 99, "https://img1.etsystatic.com/050/1/5847299/il_214x170.730058347_mt4x.jpg"),
+		createProduct(1, "Jakki",            4499,  23, 14, "http://www.newmanmayahandicraft.com.np/wp-content/uploads/2015/10/woolen-jacket-with-multicolored-design.jpg"),
+		createProduct(1, "Peysa",            5899, 122,  1, "https://upload.wikimedia.org/wikipedia/commons/7/75/Selburose-sweater.jpg"),
+		createProduct(1, "Lambhúshetta",     2499, 322,  4, "https://upload.wikimedia.org/wikipedia/commons/9/9a/Balaclava_3_hole_black.jpg"),
+		createProduct(1, "Buxur",            4299,  73,  5, ""),
+		createProduct(1, "Grifflur",         1299,  98,  9, ""),
+		createProduct(1, "Teppi",             499, 819, 98, "https://pixabay.com/static/uploads/photo/2015/11/07/14/40/fabric-1031932_960_720.jpg"),
+		createProduct(1, "Sokkar",            499, 991, 23, "https://upload.wikimedia.org/wikipedia/commons/4/42/HandKnittedWhiteLaceSock.jpg"),
+		createProduct(1, "Bindi",             899,  25, 22, ""),
+		createProduct(1, "Slaufa",            499, 552, 54, "https://pixabay.com/static/uploads/photo/2015/04/20/21/39/bow-tie-732289_960_720.jpg"),
+		createProduct(1, "Hnéháir sokkar",   2499,  93, 42, "https://upload.wikimedia.org/wikipedia/commons/c/cf/Argyle_(PSF).png"),
+		createProduct(1, "Barnatrefill",      999,  39, 11, "https://upload.wikimedia.org/wikipedia/commons/a/a4/Well-clothed_baby.jpg"),
+		createProduct(1, "Hneppt peysa",     4499,  19,  9, "https://upload.wikimedia.org/wikipedia/commons/4/4d/1940_Trachtenstrickjacke_anagoria.JPG"),
+		createProduct(1, "Hvítir vettlingar", 499, 241,  0, "https://pixabay.com/static/uploads/photo/2014/05/05/22/15/gloves-338614_960_720.jpg"),
+		createProduct(1, "Úlnliðshlífar",    1499,  34,  0, "https://pixabay.com/static/uploads/photo/2015/11/07/17/20/hands-1032312_960_720.jpg"),
+		createProduct(3, "Kókoskúlur",        499, 100, 5000, "https://upload.wikimedia.org/wikipedia/commons/2/2c/Chokladbollar.jpg"),
+		createProduct(3, "Brjóstsykur",       499, 200, 4900, ""),
 	];
 	// Note: sellers 2 and 4 don't have any products - yet!
 
@@ -111,6 +115,8 @@ function AppResource() {
 
 		addSeller: function addSeller(seller) {
 			if (mockResource.successAddSeller) {
+				sellerID += 1;
+				seller.id = sellerID;
 				mockSellers.push(seller);
 			}
 			return mockHttpPromise(mockResource.successAddSeller, seller);
@@ -118,7 +124,7 @@ function AppResource() {
 
 		updateSeller: function(id, seller) {
 			if (mockResource.successUpdateSeller) {
-				var current = null;
+				var current;
 				for(var i = 0; i < mockSellers.length; i++) {
 					if(mockSellers[i].id === id) {
 						current = mockSellers[i];
@@ -167,6 +173,8 @@ function AppResource() {
 				var seller = _.find(mockSellers, function(o){ return o.id === id;});
 				if (seller) {
 					success = true;
+					productID += 1;
+					product.id = productID;
 					mockProducts.push({
 						id: seller.id,
 						product: product
@@ -178,18 +186,17 @@ function AppResource() {
 		},
 
 		updateSellerProduct: function(id, product) {
+			var current;
 			if (mockResource.successUpdateSellerProduct) {
 				for(var i = 0; i < mockProducts.length; i++) {
-					if(mockProducts[i].product.id === id) {
-						mockProducts[i].product.name = product.name;
-						mockProducts[i].product.price = product.price;
-						mockProducts[i].product.quantitySold = product.quantitySold;
-						mockProducts[i].product.quantityInStock = product.quantityInStock;
-						mockProducts[i].product.imagePath = product.imagePath;
+					if(mockProducts[i].id === id) {
+						mockProducts[i].product = product;
+						current = mockProducts[i];
+						break;
 					}
 				}
 			}
-			return mockHttpPromise(mockResource.successUpdateSellerProduct, product);
+			return mockHttpPromise(mockResource.successUpdateSellerProduct, current);
 		}
 	};
 
